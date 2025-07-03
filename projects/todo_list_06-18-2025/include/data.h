d91a6341a4fe421f046f67140671e42e73e5f43f
Gregory Kotlyarsky <gregory.kotlyarsky@gmail.com>
2025-07-02 23:02:02 -0400

    Added to struct Data

--- projects/todo_list_06-18-2025/include/data.h
+++ projects/todo_list_06-18-2025/include/data.h
@@ -1,19 +1,27 @@
 #ifndef DATA_H
 #define DATA_H
 
+#include <time.h>
+
+#define PRIORITY_LOW 0
+#define PRIOTIRY_MEDIUM 1
+#define PRIOTIRY_HIGH 2
+
 typedef struct Data 
 {
-    /*
-    To be filled in by applicable data
-    */
+    char *taskName;
+    char *category;
+    char *taskDescription;
+    time_t inputDate;
+    time_t dueDate;
+    int priority;
+
 } Data;
 
+void getFields();
 
+void initData();
 
-
-
-
-
-
+void freeData();
 
 #endif
