0f1605500d7ceb3c13a472d9fad792e62bd1e930
Gregory Kotlyarsky <gregory.kotlyarsky@gmail.com>
2025-07-11 23:06:30 -0400

    Made function to get due date for todo list project, left redundency in case of error

--- projects/todo_list_06-18-2025/include/data.h
+++ projects/todo_list_06-18-2025/include/data.h
@@ -14,6 +14,20 @@ typedef struct Data
 
 } Data;
 
+// struct tm
+// {
+//     int tm_sec;   // seconds [0, 60]
+//     int tm_min;   // minutes [0, 59]
+//     int tm_hour;  // hours   [0, 23]
+//     int tm_mday;  // day of month [1, 31]
+//     int tm_mon;   // months since January [0, 11]
+//     int tm_year;  // years since 1900
+//     int tm_wday;  // day of week (0 = Sunday) [ignored by mktime]
+//     int tm_yday;  // day of year [ignored]
+//     int tm_isdst; // daylight saving time flag
+
+// };
+
 Data* initData();
 
 // void freeData();
--- projects/todo_list_06-18-2025/src/data.c
+++ projects/todo_list_06-18-2025/src/data.c
@@ -59,8 +59,80 @@ Data* initData() {
     // User input due date
     while (1) 
     {
-        printf("Please enter a due date (mm/dd/yyyy):");
+        int month, day, year, hour, minute;
+        int valid = 1;
+        char extra;
+        
+        printf("Please enter a due date and 24h time without any trailing spaces(mm/dd/yyyy hh:mm):\n");
+        
+        fgets(buffer, sizeof(buffer), stdin);
+        buffer[strcspn(buffer, "\n")] = '\0';
+        
+        int count = sscanf(buffer, "%d/%d/%d %d:%d%c ", &month, &day, &year, &hour, &minute, &extra);
+        
+        if (count != 5) {
+            printf("Input must be 24h time (mm/dd/yyyy hh:mm) with no trailing spaces.\n");
+            printf("Count: %d\n", count);
+            valid = 0;
+        }
+        
+        if (month < 1 || month > 12) {
+            printf("The month must be between 1-12.\n");
+            valid = 0;
+        } else if ((month == 4 || month == 6 || month == 9 || month == 11) && (day > 30)) {
+            printf("Inputed month cannot have more than 30 days.\n");
+            valid = 0;
+        } else if ((month == 2 && (year % 4 != 0)) && day > 28) {
+            printf("February cannot have more than 28 days on a normal year.\n");
+            valid = 0;
+        } else if (month == 2 && (year % 4 == 0) && day > 29) {
+            printf("February cannot have more than 29 days on a leap year.\n");
+            valid = 0;
+        } else if (day < 1 || day > 31) {
+            printf("No month can have more than 31 days or less than 0 days.\n");
+            valid = 0;
+        }
 
+        if (minute < 0 || minute > 59) {
+            printf("Minute values must be between 0 and 59.\n");
+            valid = 0;
+        }
+
+        if (hour < 0 || hour > 23) {
+            printf("Hour values must be between 0 and 23.\n");
+            valid = 0;
+        }
+
+        struct tm timeStruct = {0};
+
+        timeStruct.tm_min = minute;
+        timeStruct.tm_hour = hour;
+        timeStruct.tm_mday = day;
+        timeStruct.tm_mon = month - 1;
+        timeStruct.tm_year = year - 1900;
+        timeStruct.tm_isdst = -1;
+
+        time_t currentTime;
+        time(&currentTime);
+        
+        time_t resultDueDate = mktime(&timeStruct);
+
+        if (resultDueDate == -1) {
+            printf("Error converting time.\n");
+            valid = 0;
+        }        
+
+        if (resultDueDate < currentTime) {
+            printf("Cannot put a datetime that already passed\n");
+            valid = 0;
+        }
+
+        if (valid == 0) {
+            continue;
+        } else {
+            printf("Date accepted.\n");
+            break;
+        }
     }
 
     // Automatically enter input date
